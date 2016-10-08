56dba1d49f672a611f1878f6767f6a7d7987cbc3
Elise Harris <eharris99@yahoo.com>
2016-10-08 11:02:17 -0400

    body-parser unencoded

--- app.js
+++ app.js
@@ -1,78 +1,3 @@
-// var express = require('express');
-// var path = require('path');
-// var favicon = require('serve-favicon');
-// var logger = require('morgan');
-// var cookieParser = require('cookie-parser');
-// var bodyParser = require('body-parser');
-// var mongoose = require('mongoose');
-
-// var routes = require('./routes/index');
-// var api = require('./routes/api');
-
-// var dbUrl = 'mongodb://localhost/alzjukebox'
-// mongoose.connect(dbUrl, function(err, res){
-//   if (err){
-//     console.log('DB CONNECTION FAIL: '+err)
-//   }
-//   else {
-//     console.log('DB CONNECTION SUCCESS: '+dbUrl)
-
-//   }
-
-// })
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hjs');
-
-// // uncomment after placing your favicon in /public
-// //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', routes);
-// app.use('/api', api);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-// app.listen(8000);
-
-// module.exports = app;
-
-
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require('path');
@@ -97,7 +22,7 @@ mongoose.connect(dbUrl, function(err, res){
 
 var app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Consuming route parameters
@@ -113,9 +38,6 @@ app.get('/users/:fName lName', (req, res) => {
   res.send('The username is: ' + "   " +  req.params.fName + "   " + req.params.lName);
 });
 
-// app.get('/', function (req, res) {
-//   res.send('suh !');
-// });
 
 app.get('/', (req, res) => {
   res.send('alzjukebox homepage');
@@ -127,10 +49,6 @@ app.get('/users', (req, res) => {
   res.render('users');
 });
 
-// app.get '/', (req,res)=> {
-//   res.locals = name: 'Andrew'
-//   res.render 'template', partials: {message: 'message'}
-//   }
 
 // POST method route
 app.post('/', (req, res) => {
@@ -144,11 +62,6 @@ app.set('view engine', 'hjs');
 app.use('/', routes);
 app.use('/api', api);
 app.use('/users', users);
-
-// app.set 'layout', 'layout'        
-// app.set 'partials', foo: 'foo'   
-// app.enable 'view cache'
-// app.engine 'html', require('hogan-express')
 
 module.exports = app;
 
